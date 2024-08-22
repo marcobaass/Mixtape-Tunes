24ed4621a0418a6ed00a32fc3160ddc36df0655c
Marco Baass <picture@marcobaass.com>
2024-08-22 13:36:47 +0200

    scrollbar fixed

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -33,43 +33,42 @@ export default function Playlist({ playlistTracks, handleRemoveFromPlaylist, pla
         )
       }
 
-      <div className="-space-y-1">
-        {playlistTracks.map((track, index) => (
-          <div
-            key={index}
-            className={`${styles.playlist}`}
-          >
-            <img
-              className={`${styles.gridImg} w-9 h-9 rounded-md`}
-              src={track.image || "#"}
-              alt={track.name || 'Track Image'}
-            />
-
-            <h2 className={`${styles.gridArtist} text-hot-magenta font-bold truncate w-full max-w-[95%] text-center`}>{track.artist || 'Artist Name'}</h2>
-            <h2 className={`${styles.gridSong} text-electric-blue truncate w-full max-w-[95%] text-center`}>{track.name || 'Song Title'}</h2>
-
-            <button
-              className={`${styles.gridRemove} px-2 py-0 align-middle
-               bg-neon-pink text-white rounded-lg font-bold hover:bg-neon-purple transition-colors duration-300 text-base`}
-              onClick={() => handleRemoveFromPlaylist(track)}
+      <div className="overflow-y-auto max-h-[42vh] -space-y-1">
+        <div className="-space-y-1" >
+          {playlistTracks.map((track, index) => (
+            <div
+              key={index}
+              className={`${styles.playlist}`}
             >
-              X
-            </button>
-          </div>
-        ))}
+              <img
+                className={`${styles.gridImg} w-9 h-9 rounded-md`}
+                src={track.image || "#"}
+                alt={track.name || 'Track Image'}
+              />
+              <h2 className={`${styles.gridArtist} text-hot-magenta font-bold truncate w-full max-w-[95%] text-center`}>{track.artist || 'Artist Name'}</h2>
+              <h2 className={`${styles.gridSong} text-electric-blue truncate w-full max-w-[95%] text-center`}>{track.name || 'Song Title'}</h2>
+              <button
+                className={`${styles.gridRemove} px-2 py-0 align-middle
+                 bg-neon-pink text-white rounded-lg font-bold hover:bg-neon-purple transition-colors duration-300 text-base`}
+                onClick={() => handleRemoveFromPlaylist(track)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
+        {playlistTracks.length > 0 && !isEditing && (
+          <button onClick={() => handleSaveToSpotify(playlistTracks)} className="text-white bg-neon-pink rounded-2xl px-3 py-0.5 h-min mt-2 mr-2 hover:bg-neon-purple transition-colors duration-300 text-sm font-bold">
+            Save to Spotify
+          </button>
+        )}
+        {playlistTracks.length > 0 && (
+          <button onClick={() => getRecommendations(playlistTracks)} className="text-white bg-neon-pink rounded-2xl px-3 py-0.5 h-min mt-2 hover:bg-neon-purple transition-colors duration-300 text-sm font-bold">
+            Get recommendations
+          </button>
+        )}
+            </div>
       </div>
-      {playlistTracks.length > 0 && !isEditing && (
-        <button onClick={() => handleSaveToSpotify(playlistTracks)} className="text-white bg-neon-pink rounded-2xl px-3 py-0.5 h-min mt-2 mr-2 hover:bg-neon-purple transition-colors duration-300 text-sm font-bold">
-          Save to Spotify
-        </button>
-      )}
-
-      {playlistTracks.length > 0 && (
-        <button onClick={() => getRecommendations(playlistTracks)} className="text-white bg-neon-pink rounded-2xl px-3 py-0.5 h-min mt-2 hover:bg-neon-purple transition-colors duration-300 text-sm font-bold">
-          Get recommendations
-        </button>
-      )}
-    </div>
   );
 }
 
