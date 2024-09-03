34e70527e906d429527db7fde95f253f0df28b58
Marco Baass <picture@marcobaass.com>
2024-09-03 11:55:56 +0200

    login loading spinner

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Login from '../Login/Login'
 import App from '../App/App';
 import useAuth from '../../hooks/useAuth';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 export default function Auth() {
+  const [isLoading, setLoading] = useState(true);
   const code = new URLSearchParams(window.location.search).get('code');
-  const accessToken = useAuth(code);
+  const accessToken = useAuth(code, setLoading);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-1 h-screen justify-center items-center">
+        <LoadingSpinner /> {/* Show the loading spinner */}
+      </div>
+    );
+  }
 
   return accessToken ? <App accessToken={accessToken} /> : <Login />;
 }
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:3001';
 
-export default function useAuth(code) {
+export default function useAuth(code, setLoading) {
   const [accessToken, setAccessToken] = useState(null);
   const [refreshToken, setRefreshToken] = useState(null);
   const [expiresIn, setExpiresIn] = useState(null);
@@ -12,17 +12,25 @@ export default function useAuth(code) {
     console.log('starting auth hook');
     console.log(`${API_URL}/login`);
 
-    axios.post(`${API_URL}/login`, {
-      code,
-    }).then(res => {
-      setAccessToken(res.data.accessToken);
-      setRefreshToken(res.data.refreshToken);
-      setExpiresIn(res.data.expiresIn);
-      window.history.pushState({}, null, '/');
-    }).catch(() => {
-      // window.location = '/';
-    })
-  }, [code]);
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.post(`${API_URL}/login`, {
+          code,
+        });
+          setAccessToken(response.data.accessToken);
+          setRefreshToken(response.data.refreshToken);
+          setExpiresIn(response.data.expiresIn);
+          window.history.pushState({}, null, '/');
+      } catch (error) {
+          console.log('Error during Spotify login:', error);
+      } finally {
+          setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [code, setLoading]);
 
   useEffect(() => {
     if (!refreshToken ||!expiresIn) return;
