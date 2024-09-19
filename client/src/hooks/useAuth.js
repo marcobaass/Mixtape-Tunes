ee1d29ff53c265659f5d42d5e71ff51ddbe4079e
Marco Baass <picture@marcobaass.com>
2024-09-19 14:32:50 +0200

    expiration

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -69,6 +69,7 @@ export default function useAuth(code, setLoading) {
         console.log('Refreshing access token in useAuth...');
         console.log('Refresh Token: ', refreshToken);
         console.log('Expiration: ', expiresIn);
+        console.log(`${API_URL}/refresh`, { refreshToken });
         const response = await axios.post(`${API_URL}/refresh`, { refreshToken });
         const { accessToken, expiresIn } = response.data;
 
