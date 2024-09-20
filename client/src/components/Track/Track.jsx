a82a4171dfc610eaa577c378328fdab6e6011136
Marco Baass <picture@marcobaass.com>
2024-09-20 15:16:28 +0200

    spotify logo

--- client/src/components/Track/Track.jsx
+++ client/src/components/Track/Track.jsx
@@ -39,10 +39,11 @@ export default function Track({ track, handleAddToPlaylist, handlePlay, isPlayin
       <h2 className={`${styles.gridSong} text-black truncate w-full max-w-[95%] text-center`}>{track.name || 'Song Title'}</h2>
       <h2 className={`${styles.gridAlbum} text-white truncate w-full max-w-[95%] text-center`}>{track.album || 'Album Title'}</h2>
 
-      <Link to={`${track.external_url}`} className={`${styles.gridSpot} object-cover rounded-xl p-2`}>
+      <Link to={`${track.external_url}`} className={`${styles.gridSpot}`}>
         <img
           src="/imgs/Spotify_Primary_Logo_RGB_Green.png"
           alt="Link to Spotify"
+          className="w-full h-full object-cover"
         />
       </Link>
 
