9a5184a3ad3ca8b11b1c9340703dc2757074c164
Marco Baass <picture@marcobaass.com>
2024-11-27 11:44:27 +0100

    login when wrong accessToken

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -219,6 +219,22 @@ function App({accessToken, loginRef}) {
       setSuggestions(fetchedSuggestions);
     } catch (error) {
       console.error("Error fetching suggestions:", error);
+      loginRef.current = false;
+
+        window.localStorage.removeItem('accessToken');
+        window.localStorage.removeItem('spotify_access_token');
+        localStorage.clear();
+        window.location.reload(true);
+        console.log('Access token after wipe 1', accessToken);
+        window.localStorage.removeItem('refreshToken');
+        window.localStorage.removeItem('expiresIn');
+
+        window.sessionStorage.removeItem('accessToken');
+        console.log('Access token after wipe 2', accessToken);
+        window.sessionStorage.removeItem('refreshToken');
+
+        console.log('Navigating to login page...');
+        navigate('/login');
     } finally {
       setLoading(false); // Set loading to false
     }
